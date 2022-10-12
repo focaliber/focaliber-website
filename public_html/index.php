8edeb004a65cdea8d5554319702e085901ee984c
Gerwin Jansen <gerwinjansen@users.noreply.github.com>
2022-10-12 22:10:33 +0200

    Deploy to hosting provider

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,4 @@
 <?php
-/**
- * WordPress View Bootstrapper
- */
-define('WP_USE_THEMES', true);
-require __DIR__ . '/core/wp-blog-header.php';
+header('Location: https://www.facebook.com/Focaliber');
+die();
+?>
